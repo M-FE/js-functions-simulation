021de140e935ea2e47b72c30c4927150997c8f73
willemwei023@gmail.com <willemwei023@gmail.com>
2020-08-24 22:21:41 +0800

    Throttle - add Cancel, Pending

--- src/throttle.ts
+++ src/throttle.ts
@@ -8,24 +8,37 @@ function throttle(func: Function, wait: number = 0, options: ThrottleOptions) {
     let timer: NodeJS.Timeout | null;
     let previous: number = leading ? 0 : +new Date();
 
+    const callFunc = (context: Function, args: any[]) => {
+        func.apply(context, args);
+        previous = +new Date();
+        timer = null;
+    }
+
     const throttled = function(this: Function, ...args: any[]) {
         const now = +new Date();
+        const remaining = wait - (now - previous);
 
-        if (now - previous > wait) {
+        if (remaining <= 0) {
             if (timer) {
                 clearTimeout(timer);
-                timer = null;
             }
 
-            func.apply(this, args);
-            previous = now;
+            callFunc(this, args);
         } else if (!timer && trailing) {
             timer = setTimeout(() => {
-                func.apply(this, args);
-                previous = now;
-                timer = null;
-            }, wait);
+                callFunc(this, args);
+            }, remaining);
         }
+    }
+
+    throttled.cancel = function() {
+        timer && clearTimeout(timer);
+        timer = null;
+        previous = 0;
+    };
+
+    throttled.pending = function() {
+        return !!timer || (+new Date() - previous <= wait);
     }
 
     return throttled;
